0d3eecfab70cbc76d613554c4d3615ad7c4e42f1
Hari Prasad SA <sahariprasad@gmail.com>
2021-05-12 16:16:55 +0530

    - Removed function call

--- main.py
+++ main.py
@@ -90,6 +90,3 @@ def make_models(input_file_path, output_file_path):
     output_file = open(model_output_path + "\\schema.yml", "w", encoding="utf-8")
     output_file.write(schema_str)
     output_file.close()
-
-
-make_models(r"C:\Users\sahar\Desktop\dbt-starter", r"C:\Users\sahar\Desktop\dbt-starter\models")
